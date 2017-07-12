2245bead9b6ea798286464119fb7560deaecc707
Joaquin Peña <daniel_master5@hotmail.com>
2017-07-12 09:55:20 -0300

    cambios menores

--- PushNotificaciones/src/munijujuy/com/Vistas/MainActivity.java
+++ PushNotificaciones/src/munijujuy/com/Vistas/MainActivity.java
@@ -6,18 +6,20 @@ import com.parse.ParseAnalytics;
 
 
 
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-
+import android.widget.Button;
 import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
 	
+	private Button btnWeb,btnMap,btnAfiliado,btnEvento,btnCalendario,btnContacto;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +27,77 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		ParseAnalytics.trackAppOpened(getIntent());
 		
+		btnAfiliado = (Button)findViewById(R.id.buttonRadio);
+		//btnMap = (Button)findViewById(R.id.buttonplay);
+		btnWeb = (Button)findViewById(R.id.buttonweb);
+		btnEvento = (Button)findViewById(R.id.buttonEvento);
+		btnCalendario = (Button)findViewById(R.id.buttonCalendario);
+		btnContacto = (Button)findViewById(R.id.buttonContacto);
+		
+		
 		//myWebView = (WebView) findViewById(R.id.webview);
 		//WebSettings webSettings = myWebView.getSettings();
 		
 		//webSettings.setJavaScriptEnabled(true);
       //  webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
        // myWebView.loadUrl("http://www.sansalvadordejujuy.gob.ar/");
+		btnAfiliado.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				//Toast toast1 = Toast.makeText(getApplicationContext(),"Disculpe no esta disponible hasta su lanzamiento", Toast.LENGTH_SHORT);
+		 	    //toast1.show();  
+				Intent intent = new Intent(MainActivity.this, ContactActivity.class);
+				
+				startActivityForResult(intent,2);
+			}
+		});
+		
+		/*btnMap.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(MainActivity.this, MapaActivity.class);
+				startActivityForResult(intent,2);
+			}
+		});*/
+		
+		btnWeb.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(MainActivity.this, WebActivity.class);
+				
+				startActivity(intent);
+			}
+		});
+		btnEvento.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(MainActivity.this, AddEventActivity.class);
+				startActivity(intent);
+			}
+		});
+		btnCalendario.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(MainActivity.this, EventListActivity.class);
+				startActivity(intent);
+			}
+		});
+		btnContacto.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(MainActivity.this, ContactActivity.class);
+				
+				startActivityForResult(intent,2);
+			}
+		});
 	}
 
 	@Override
@@ -53,7 +120,7 @@ public class MainActivity extends Activity {
 	}
 	
 	
-	
+	/*
 	public void btnPlay (View view)
 	{
 		
@@ -77,7 +144,7 @@ public class MainActivity extends Activity {
 		
 	}
 	
-	
+	*/
 	
 	public void btnMap (View view)
 	{
@@ -88,7 +155,7 @@ public class MainActivity extends Activity {
 		
 	}
 	
-	
+	/*
 	public void btnweb (View view)
 	{
 		
@@ -115,7 +182,7 @@ public class MainActivity extends Activity {
 		startActivity(intent);
 		
 	}
-	
+	*/
 	
 	
 	@Override
