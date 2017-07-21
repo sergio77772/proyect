795fea4dff87ea8a2d4eff718ccd0748bbaa1573
Joaquin Peña <daniel_master5@hotmail.com>
2017-07-21 00:39:27 -0300

    seccion camara funcionando

--- PushNotificaciones/src/munijujuy/com/Vistas/Config.java
+++ PushNotificaciones/src/munijujuy/com/Vistas/Config.java
@@ -2,7 +2,9 @@ package munijujuy.com.Vistas;
 
 public class Config {
 	// url del host de imagenes (replace the ip with your server address)
-    public static final String FILE_UPLOAD_URL = "http://192.168.0.104/AndroidFileUpload/fileUpload.php";
+    //public static final String FILE_UPLOAD_URL = "http://192.168.0.104/AndroidFileUpload/fileUpload.php";
+	//http://www.comitecapitaljujuy.com/MOVIL
+	public static final String FILE_UPLOAD_URL = "http://www.comitecapitaljujuy.com/MOVIL/fileUpload.php";
      
     // Directory name to store captured images and videos
     public static final String IMAGE_DIRECTORY_NAME = "Android File Upload";
